9e76bff58d4a0b35a03b68eda3b3dcd2fd5e669d
haydar taskin <tknhaydar@gmail.com>
2018-04-13 15:09:14 +0200

    transition slide for home and index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+import Flickity from 'flickity';
+import 'flickity/dist/flickity.min.css';
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log("DOM is loaded and we are ready to Flickity!");
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    // options
+    contain: true
+  });
+})
