8dbc8bfcfb537744602c5ab01d7ef7d1d5993d8a
JJ Bernard <jjulien@kombolo.ooo>
2018-04-17 15:58:10 +0200

    design sign pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,6 @@ Content.destroy_all
 youtube_contents_attributes = JSON.parse(File.open('db/fixtures/youtube.json').read)
 youtube_contents_attributes.each { |content_attributes| Content.create(content_attributes) }
 
-
-
-
-
-
-
-
 medium_content_attributes = {
 
  kind: "article",
